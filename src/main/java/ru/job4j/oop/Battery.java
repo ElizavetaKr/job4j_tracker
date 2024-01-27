1c4d35e4fcd38a2c002b3a0c132970ecddb8f4db
ElizavetaKr <122363218+ElizavetaKr@users.noreply.github.com>
2024-01-27 16:30:55 +0300

    1.8. Класс использующий сам себя [#167388 #476774]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -3,7 +3,7 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
-    Battery(int load) {
+    public Battery(int load) {
         this.load = load;
     }
 
@@ -12,8 +12,8 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-    another.load += this.load;
-    this.load = 0;
+        another.load += this.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
