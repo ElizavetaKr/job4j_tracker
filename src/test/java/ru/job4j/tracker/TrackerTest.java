ec3b805921336a2917b63e9a134066c291a6f9a8
ElizavetaKr <elizovetakryukova@mail.ru>
2024-02-02 09:51:01 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #478393]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -4,6 +4,16 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TrackerTest {
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        item.setName("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName()).isEqualTo(item.getName());
+    }
     @Test
     public void whenTestFindById() {
         Tracker tracker = new Tracker();
