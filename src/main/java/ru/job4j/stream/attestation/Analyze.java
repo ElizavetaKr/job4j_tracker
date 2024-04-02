9ef478402bc9d179d3bc1d5d7995784b9285eb35
ElizavetaKr <elizovetakryukova@mail.ru>
2024-04-02 09:35:09 +0300

    3. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/attestation/Analyze.java
+++ src/main/java/ru/job4j/stream/attestation/Analyze.java
@@ -50,6 +50,7 @@ public class Analyze {
                 .collect(
                         Collectors.groupingBy(
                                 Subject::name,
+                                LinkedHashMap::new,
                                 Collectors.averagingDouble(Subject::score)
                         )
                 )
@@ -88,7 +89,6 @@ public class Analyze {
                 .collect(
                         Collectors.groupingBy(
                                 Subject::name,
-                                LinkedHashMap::new,
                                 Collectors.summingDouble(Subject::score)
                         )
                 )
