62cf152d04e5841e029ae69312309d8977de8f30
ElizavetaKr <elizovetakryukova@mail.ru>
2024-10-20 11:15:20 +0300

    2. Трекер SQL [#1734 #518154]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -3,10 +3,8 @@ package ru.job4j.tracker;
 import ru.job4j.tracker.Item;
 
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -48,9 +46,18 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute(String.format("INSERT INTO items(id, name, created) VALUES (%s, '%s', '%s');",
-                    item.getId(), item.getName(), item.getCreated()));
+        try (PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO items(name, created) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, item.getName());
+            Timestamp timestampFromLDT = Timestamp.valueOf(item.getCreated());
+            statement.setTimestamp(2, timestampFromLDT);
+            statement.execute();
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -63,9 +70,11 @@ public class SqlTracker implements Store {
             return false;
         }
         try (Statement statement = connection.createStatement()) {
-            statement.execute(String.format("UPDATE items SET name = %s, created = %s"
+            Timestamp timestampFromLDT = Timestamp.valueOf(item.getCreated());
+            statement.execute(String.format("UPDATE items SET name = '%s', created = '%s'"
                             + "WHERE id = %s;",
-                    item.getName(), item.getCreated(), id));
+                    item.getName(), timestampFromLDT, id));
+            item.setId(id);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -92,8 +101,9 @@ public class SqlTracker implements Store {
             var selection = statement.executeQuery(String.format(
                     "SELECT * FROM items"));
             while (selection.next()) {
-                items.add(new Item(selection.getInt("id"),
-                        selection.getString("name")));
+                items.add(create(selection.getInt("id"),
+                        selection.getString("name"),
+                        selection.getTimestamp("created")));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -108,8 +118,9 @@ public class SqlTracker implements Store {
             var selection = statement.executeQuery(String.format(
                     "SELECT * FROM items WHERE name = '%s'", key));
             while (selection.next()) {
-                items.add(new Item(selection.getInt("id"),
-                        selection.getString("name")));
+                items.add(create(selection.getInt("id"),
+                        selection.getString("name"),
+                        selection.getTimestamp("created")));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -119,17 +130,23 @@ public class SqlTracker implements Store {
 
     @Override
     public Item findById(int id) {
-        Item item = new Item();
+        Item item = null;
         try (var statement = connection.createStatement()) {
             var selection = statement.executeQuery(String.format(
                     "SELECT * FROM items WHERE id = %s", id));
             if (selection.next()) {
-                item = new Item(selection.getInt("id"),
-                        selection.getString("name"));
+                item = create(selection.getInt("id"),
+                        selection.getString("name"),
+                        selection.getTimestamp("created"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return item;
+    }
+
+    private Item create(int id, String name, Timestamp created) {
+        LocalDateTime localDateTime = created.toLocalDateTime();
+        return new Item(id, name, localDateTime);
     }
 }
